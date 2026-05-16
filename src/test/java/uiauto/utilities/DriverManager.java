57504949fb5a1b34e11d5bcef2466694ea795247
Hakiki Fajars <hakikifajarsidiq@gmail.com>
2026-05-16 15:01:51 +0700

    Fix Selenium headless mode for GitHub Actions

--- src/test/java/uiauto/utilities/DriverManager.java
+++ src/test/java/uiauto/utilities/DriverManager.java
@@ -3,6 +3,7 @@ package uiauto.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -14,9 +15,22 @@ public class DriverManager {
         if (driver == null) {
             WebDriverManager.chromedriver().setup();
 
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
+            ChromeOptions options = new ChromeOptions();
+
+            if ("true".equals(System.getenv("CI"))) {
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--disable-gpu");
+                options.addArguments("--window-size=1920,1080");
+            }
+
+            driver = new ChromeDriver(options);
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            if (!"true".equals(System.getenv("CI"))) {
+                driver.manage().window().maximize();
+            }
         }
 
         return driver;
